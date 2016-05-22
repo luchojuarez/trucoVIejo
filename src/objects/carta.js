daede4b9b08acba45a17aecb4c23a70135ed189b
Rodrigo Grazini <grazini.rodrigo@gmail.com>
2016-05-21 21:22:03 -0300

    Modificado cartas.js

--- src/objects/carta.js
+++ src/objects/carta.js
@@ -1,30 +1,24 @@
+var weight = {
+        "Copa":   [ 8, 9, 10, 1, 2, 3,  4, 0, 0, 5, 6, 7],
+        "Espada": [14, 9, 10, 1, 2, 3, 12, 0, 0, 5, 6, 7],
+        "Oro":    [ 8, 9, 10, 1, 2, 3, 11, 0, 0, 5, 6, 7],
+        "Basto":  [13, 9, 10, 1, 2, 3,  4, 0, 0, 5, 6, 7]
+}
 
 function Carta() {
     this.palo = null;
     this.numero = null;
+	this.peso = 0;
 }
 
 
 function Carta(palo,numero) {
     this.palo = palo;
     this.numero = numero;
-    this.peso = this.getPeso();
+    this.peso = weight[palo][numero-1];
 }
 
-Carta.prototype.getPeso = function() {
-    var weight = {
-        "Copa":   [ 8, 9, 9, 1, 2, 3,  4, 0, 0, 5, 6, 7],
-        "Espada": [14, 9, 9, 1, 2, 3, 11, 0, 0, 5, 6, 7],
-        "Oro":    [ 8, 9, 9, 1, 2, 3, 10, 0, 0, 5, 6, 7],
-        "Basto":  [13, 9, 9, 1, 2, 3,  4, 0, 0, 5, 6, 7]
-    };
-    return this.palo && this.numero ? weight[this.palo][this.numero] : 0;
-}
-
-Carta.prototype.peso;
-Carta.prototype.palo;
-Carta.prototype.numero;
-
+Carta.prototype.getPeso = function() {return this.peso};
 Carta.prototype.getPalo = function() {return this.palo;};
 Carta.prototype.getNumero = function() {return this.numero;};
 
@@ -42,7 +36,7 @@ Carta.prototype.getPuntos = function() {
         return this.numero;
     }
     else {
-        return 10;
+        return 0;
     }
 }
 
