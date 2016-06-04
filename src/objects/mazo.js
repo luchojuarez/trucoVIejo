7d342b7da0787ed2731ae6ecde0c3ef3fe8b92c9
Rodrigo Grazini <grazini.rodrigo@gmail.com>
2016-06-04 20:38:18 -0300

    Modificar mazo.js
    Remover las funciones definidas de mas para el objeto mazo

--- src/objects/mazo.js
+++ src/objects/mazo.js
@@ -2,56 +2,28 @@ var _ = require('lodash');
 var moduloCarta = require("../objects/carta.js");
 var Carta = moduloCarta.carta;
 
-
 var suits = ['Oro', 'Copa', 'Espada', 'Basto'];
 var cardNumbers = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
 
+
+//El objeto mazo tiene 2 funciones que retornar un arreglo de cartas, cada carta es generada con una combinacion entre suits y cardNumbers
 function Mazo () {
-//crear un nuevo mazo sin cartas
-    cartas = this.mix();
-}
-
-Mazo.prototype.cartas;
-
-Mazo.prototype.init = function () {
-//Crea un mazo con las cartas correspondientes a suits y cardNumbers
-    var c = suits.length * cardNumbers.length;
-    var i,j;
-    this.cartas = new Array(c);
-    for (i=0;i< suits.length;i++) {
-	for (j=0;j < cardNumbers.length;j++) {
-		this.cartas[i*suits.length + j] = new Carta(suits[i],cardNumbers[j]);
-	}
-    }
-}
-
-Mazo.prototype.darCarta = function () {
-//Obtener la primera carta del mazo, se modifica el arreglo cartas
-    if (this.cartas.length > 0)
-	return  this.cartas.shift();
-    else
-	return null;
-}
-
-Mazo.prototype.cantCartas = function () {
-//retorna la cantidad de elementos en cartas
-    return this.cartas.length;
 }
 
 
-Mazo.prototype.addCarta = function (carta) {
-//agrega una carta al tope del mazo
-    this.cartas.push(carta);
-}
 
+//Output: Arreglo de @Object Carta, donde cada carta tiene los valores
+//suits x cardNumbers (producto cartesiano)
 Mazo.prototype.sorted = function(){
-  return _.flatten( _.map(suits, function(suit){
-                      return _.map(cardNumbers, function(number){
-                        return new Carta(suit,number);
-                      });
-                    }));
+	return _.flatten( _.map(suits, function(suit){
+		return _.map(cardNumbers, function(number){
+			return new Carta(suit,number);
+		});
+	}));
 };
 
+//Output: Mezcla el arreglo obtenido con sorted y lo retorna
+//Retorna un mazo mezclado
 Mazo.prototype.mix = function(){
   return _.shuffle(this.sorted());
 };
