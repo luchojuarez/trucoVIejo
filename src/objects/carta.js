a654e436daee864c715e355226cbdf479427240f
Rodrigo Grazini <grazini.rodrigo@gmail.com>
2016-06-04 20:37:13 -0300

    Fixed carta.js
    Cambia la forma que accede a la matriz de pesos.
    Arregla getPuntos de una carta para las cartas negras

--- src/objects/carta.js
+++ src/objects/carta.js
@@ -1,31 +1,25 @@
+var weigth = {
+        "Copa":   [ 8, 9, 10, 1, 2, 3,  4, 0, 0, 5, 6, 7],
+        "Espada": [14, 9, 10, 1, 2, 3, 12, 0, 0, 5, 6, 7],
+        "Oro":    [ 8, 9, 10, 1, 2, 3, 11, 0, 0, 5, 6, 7],
+        "Basto":  [13, 9, 10, 1, 2, 3,  4, 0, 0, 5, 6, 7]
+}
 
 function Carta() {
     this.palo = null;
     this.numero = null;
+	this.peso = 0;
 }
 
 
 function Carta(palo,numero) {
     this.palo = palo;
     this.numero = numero;
-    this.peso = this.getPeso();
-}
-
-Carta.prototype.getPeso = function() {
-    var weight = {
-        "Copa":   [ 8, 9, 9, 1, 2, 3,  4, 0, 0, 5, 6, 7],
-        "Espada": [14, 9, 9, 1, 2, 3, 11, 0, 0, 5, 6, 7],
-        "Oro":    [ 8, 9, 9, 1, 2, 3, 10, 0, 0, 5, 6, 7],
-        "Basto":  [13, 9, 9, 1, 2, 3,  4, 0, 0, 5, 6, 7]
-    };
-    return this.palo && this.numero ? weight[this.palo][this.numero-1] : 0;
+    this.peso = weigth[palo][numero-1];
 }
 
 
-Carta.prototype.peso;
-Carta.prototype.palo;
-Carta.prototype.numero;
-
+Carta.prototype.getPeso = function() {return this.peso};
 Carta.prototype.getPalo = function() {return this.palo;};
 Carta.prototype.getNumero = function() {return this.numero;};
 
@@ -43,8 +37,19 @@ Carta.prototype.getPuntos = function() {
         return this.numero;
     }
     else {
-        return 10;
+        return 0;
     }
 }
 
+/*
+   Enfrentar esta carta contra otra
+output:
+   -negativo= la segunda le gana a esta
+   -postivo= esta le gana a la segunda
+   -0= Empate
+*/
+Carta.prototype.enfrentar = function(carta) {
+    var res = this.peso - carta.peso;
+    return res;
+}
 module.exports.carta = Carta;
